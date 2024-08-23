86a9a4d033506e8a5bf49317e961fe01150858bf
Jubin <jubinphilip25@gmail.com>
2024-08-23 17:06:42 +0530

    commit

--- src/components/Subscribe/Sub.tsx
+++ src/components/Subscribe/Sub.tsx
@@ -2,16 +2,32 @@ import './sub.css'
 import React, { useState } from 'react'
 
 function Subscribe() {
+    
     type Record={
         name:string,
         email:string
     }
-    const [record,setRecord]=useState({})
+    const [error,setError]=useState<string>("")
+    const [record,setRecord]=useState({username:"" ,useremail:""})
     function handleChange(e:React.ChangeEvent<HTMLInputElement>)
     {
         e.preventDefault()
         setRecord((prev)=>({...prev,[e.target.name]:e.target.value}))
         console.log(record)
+    }
+    function formValidate()
+    {
+        console.log(record.username)
+        const usernameRegex = /^[A-Za-z\s]+$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        if (!usernameRegex.test(record.username) || record.username.trim() === '') {
+            setError('Username should only contain letters and spaces and not empty.')
+        }
+        else if (!emailRegex.test(record.useremail) || record.useremail.trim()==='' ) 
+        {
+            setError('Invalid email address.')
+        }
+        console.log(record.useremail)
     }
   return (
     <div>
@@ -22,11 +38,11 @@ function Subscribe() {
             Subscribe here for exclusive offers and updates!
         </h4>
         <form className="subform" id="subForm">
-            <input type="text" id="username" name='username' placeholder="Name" onChange={handleChange}  required/>
-            <input type="email" id="usermail" name='useremail' placeholder="Email" onChange={handleChange} required/><br/>
-            <span id="emailSpan" ></span><br/>
-            <span>Don&apost miss out! enter your email and your name, then hit subscribe to unlock a world of special offers and details</span><br/>
-            <button >Subscribe</button>
+            <input type="text" id="username" name='username' placeholder="Name" onChange={handleChange}  />
+            <input type="email" id="usermail" name='useremail' placeholder="Email" onChange={handleChange} /><br/>
+            <span id="emailSpan" >{error?error:null}</span><br/>
+            <span>Dont miss out! enter your email and your name, then hit subscribe to unlock a world of special offers and details</span><br/>
+            <button onClick={formValidate}>Subscribe</button>
         </form>
     </div>
     <div className="sub-part2">
