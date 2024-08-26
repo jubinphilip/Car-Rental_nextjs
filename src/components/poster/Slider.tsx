cbe5db3ba5128ef81be05fee4d9c297d9a70e235
Jubin <jubinphilip25@gmail.com>
2024-08-26 09:57:13 +0530

    commit

--- src/components/poster/Slider.tsx
+++ src/components/poster/Slider.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './slider.css'
+
+// Array of dummy images
+const images = [
+  "/assets/poster.svg",
+  "/assets/poster.svg",
+  "/assets/poster.svg",
+  "/assets/poster.svg",
+];
+
 function Slider() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [nextImageIndex, setNextImageIndex] = useState(1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setNextImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-         <div className="poster">
-    <div className="poster-txt">
-        <p>Recieve guarnteed <button>GIFT VOUCHER</button><br/>on a monthly hire or lease
-        </p>
-    </div>
-    </div>
+      <div className="poster">
+        <div className="slider-container">
+          <img
+            src={images[currentImageIndex]}
+            alt={`Poster ${currentImageIndex + 1}`}
+            className="poster-image current"
+          />
+          <img
+            src={images[nextImageIndex]}
+            alt={`Poster ${nextImageIndex + 1}`}
+            className="poster-image next"
+          />
+        </div>
+        <div className="poster-txt">
+          <p>Receive guaranteed <button>GIFT VOUCHER</button><br/>on a monthly hire or lease
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
